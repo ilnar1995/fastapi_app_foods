c9350856659dd7f6ced2cee5831f18a33067b56e
ilnar iskhakov <ilnar150595@gmail.com>
2023-07-01 17:36:00 +0300

    обновлен путь declarative_base

--- src/core/db.py
+++ src/core/db.py
@@ -2,21 +2,17 @@ from typing import AsyncGenerator
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from starlette.requests import Request
 from fastapi import Depends
 from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
 SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:123456@localhost/microblog"
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 # Dependency
